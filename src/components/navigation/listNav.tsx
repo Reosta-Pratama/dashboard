f0f866bd5d5ab13937eecd757b16bd8be134b80a
Reosta Bayu Pratama Pane <reosta.pratama10@gmail.com>
2024-02-26 06:04:24 +0700

    Main

--- src/components/navigation/listNav.tsx
+++ src/components/navigation/listNav.tsx
@@ -1,45 +1,149 @@
 import React from 'react'
 import Navigation from './nav'
-import { FiCalendar, FiCreditCard, FiFileText, FiHardDrive, FiHome, FiInbox, FiMessageSquare, FiShoppingBag } from 'react-icons/fi'
+import {
+    FiCalendar,
+    FiCreditCard,
+    FiEdit,
+    FiFileText,
+    FiGrid,
+    FiHardDrive,
+    FiHome,
+    FiInbox,
+    FiLayout,
+    FiMessageSquare,
+    FiSettings,
+    FiShoppingBag,
+    FiSidebar,
+    FiUser,
+    FiUsers
+} from 'react-icons/fi'
 import Divider from '../divider'
 
 const ListNav = () => {
     return (
         <nav>
             <ul className='flex flex-col gap-1'>
-                <Navigation name="dashboard" href="/" >
-                    <FiHome />
+                <Navigation name="dashboard" href="/">
+                    <FiHome/>
                 </Navigation>
 
-                <Navigation name='e-commerce' href='/e-commerce' dropdown="yes">
-                    <FiShoppingBag />
+                <Navigation
+                    name='e-commerce'
+                    href='/e-commerce'
+                    dropdown="yes"
+                    droplink={[
+                                ['Categories', '/e-commerce-categories'],
+                                ['Add Product', '/e-commerce-add-product'],
+                                ['product list', '/e-commerce-product-list'],
+                                ['transaction list', '/e-commerce-transaction-list'],
+                                ['seller list', '/e-commerce-seller-list'],
+                                ['seller detail', '/e-commerce-seller-detail'],
+                                ['review', '/e-commerce-review'],
+                    ]}>
+                    <FiShoppingBag/>
                 </Navigation>
 
-                <Navigation name="inbox" href="/inbox" >
-                    <FiInbox />
+                <Navigation name="inbox" href="/inbox">
+                    <FiInbox/>
                 </Navigation>
 
-                <Navigation name="file manager" href="/file-manager" >
-                    <FiHardDrive />
+                <Navigation name="file manager" href="/file-manager">
+                    <FiHardDrive/>
                 </Navigation>
 
-                <Navigation name="poin of sale" href="/poin-of-sale" >
-                    <FiCreditCard />
+                <Navigation name="poin of sale" href="/poin-of-sale">
+                    <FiCreditCard/>
                 </Navigation>
 
-                <Navigation name="chat" href="/chat" >
-                    <FiMessageSquare />
+                <Navigation name="chat" href="/chat">
+                    <FiMessageSquare/>
                 </Navigation>
 
-                <Navigation name="post" href="/post" >
-                    <FiFileText />
+                <Navigation name="post" href="/post">
+                    <FiFileText/>
                 </Navigation>
 
-                <Navigation name="calender" href="/calender" >
-                    <FiCalendar />
+                <Navigation name="calender" href="/calender">
+                    <FiCalendar/>
                 </Navigation>
 
                 <Divider></Divider>
+
+                <Navigation name = 'crud' href = '/crud' dropdown = "yes"
+                    droplink={[
+                        ['data list', '/crud-data-list'],
+                        ['form', '/crud-form'],
+                    ]}>
+                    <FiEdit/>
+                </Navigation>
+
+                <Navigation name='users' href='/users'>
+                    <FiUsers/>
+                </Navigation>
+
+                <Navigation name='profile' href='/profile'>
+                    <FiUser/>
+                </Navigation>
+
+                <Navigation name='pages' href='/pages' dropdown="yes"
+                    droplink={[
+                        ['wizards', '/pages-wizards'],
+                        ['blog', '/pages-blog'],
+                        ['pricing', '/pages-pricing'],
+                        ['invoice', '/pages-invoice'],
+                        ['FAQ', '/pages-faq'],
+                        ['login', '/pages-login'],
+                        ['register', '/pages-register'],
+                        ['error', '/pages-error'],
+                        ['update profile', '/pages-update-profile'],
+                        ['change password', '/pages-change-password'],
+                    ]}>
+                    <FiLayout/>
+                </Navigation>
+
+                <Divider></Divider>
+
+                <Navigation name='components' href='/components' dropdown="yes"
+                    droplink={[
+                        ['table', '/components-table'],
+                        ['tabulator', '/components-tabulator'],
+                        ['modal', '/components-modal'],
+                        ['slide over', '/components-slide-over'],
+                        ['notification', '/components-notification'],
+                        ['tab', '/components-tab'],
+                        ['accordion', '/components-accordion'],
+                        ['button', '/components-button'],
+                        ['alert', '/components-alert'],
+                        ['progress bar', '/components-progress-bar'],
+                        ['tooltip', '/components-tooltip'],
+                        ['dropdown', '/components-dropdown'],
+                        ['typography', '/components-typography'],
+                        ['icon', '/components-icon'],
+                        ['loading icon', '/components-loading-icon'],
+                    ]}>
+                    <FiGrid/>
+                </Navigation>
+
+                <Navigation name='forms' href='/forms' dropdown="yes"
+                    droplink={[
+                        ['regular form', '/forms-regular-form'],
+                        ['datepicker', '/forms-datepicker'],
+                        ['tom select', '/forms-tom-select'],
+                        ['file upload', '/forms-file-upload'],
+                        ['wysiwyg editor', '/forms-wysiwyg-editor'],
+                        ['validation', '/forms-validation'],
+                    ]}>
+                    <FiSidebar/>
+                </Navigation>
+
+                <Navigation name='widgets' href='/widgets' dropdown="yes"
+                    droplink={[
+                        ['charts', '/widgets-charts'],
+                        ['slider', '/widgets-slider'],
+                        ['image zoom', '/widgets-image-zoom'],
+                    ]}>
+                    <FiSettings/>
+                </Navigation>
             </ul>
         </nav>
     )
--- src/components/navigation/nav.tsx
+++ src/components/navigation/nav.tsx
@@ -3,15 +3,17 @@
 import Link from 'next/link';
 import React, { ReactNode, useState } from 'react';
 import { FiChevronDown } from 'react-icons/fi';
+import DropNav from './dropNav';
 
 interface NavigationProps {
     name: string;
     href: string;
     dropdown?: string | null;
+    droplink?: Array<Array<string>> | null;
     children: ReactNode;
-}
+  }
 
-const Navigation: React.FC<NavigationProps> = ({ name, href, dropdown, children }) => {
+const Navigation: React.FC<NavigationProps> = ({ name, href, dropdown, droplink, children }) => {
     const [isDropdownOpen, setisDropdownOpen] = useState(false)
 
     const toggleDropdown = () => {
@@ -23,7 +25,7 @@ const Navigation: React.FC<NavigationProps> = ({ name, href, dropdown, children 
         transition: 'transform 0.3s ease',
     }
 
-  return (
+    return (
     <li>
         <Link href={href}
             onClick={(e) => {
@@ -34,17 +36,19 @@ const Navigation: React.FC<NavigationProps> = ({ name, href, dropdown, children 
             }}
             className='group relative h-12 flex items-center 
                 rounded-full pl-5 gap-3 text-white'>
-        <span className='text-xl'>{children}</span>
-        <div className='flex flex-1 items-center capitalize'>
-            {name}
-            {dropdown !== undefined && (
-                <span className='text-base ml-auto mr-5' style={iconStyle}><FiChevronDown /></span>
-            )}
-        </div>
+            <span className='text-xl'>{children}</span>
+            <div className='flex flex-1 items-center capitalize'>
+                {name}
+                {dropdown !== undefined && (
+                    <span className='text-base ml-auto mr-5' style={iconStyle}><FiChevronDown /></span>
+                )}
+            </div>
 
-        <div className="absolute -z-[1] top-0 left-0 w-[230px] h-full rounded-l-full
-            duration-300 ease-in-out group-hover:bg-[#ffffff0d]"></div>
+            <div className="absolute -z-[1] top-0 left-0 w-[230px] h-full rounded-l-full
+                duration-300 ease-in-out group-hover:bg-[#ffffff0d]"></div>
         </Link>
+
+        <DropNav data={droplink}></DropNav>
     </li>
   );
 }
